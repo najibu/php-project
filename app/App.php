70e7a86f6538bc897da1f67ed6b70517b6ffc890
Najibu <najibu.nsubuga@vice.com>
2025-02-18 15:16:59 +0000

    add twig

--- app/App.php
+++ app/App.php
@@ -4,15 +4,18 @@ declare(strict_types = 1);
 
 namespace App;
 
-use Dotenv\Dotenv;
-use Illuminate\Events\Dispatcher;
-use Illuminate\Container\Container;
-use App\Services\AbstractApi\EmailValidationService;
-use App\Exceptions\RouteNotFoundException;
 use App\Contracts\EmailValidationInterface;
-use Symfony\Component\Mailer\MailerInterface;
-use Illuminate\Database\Capsule\Manager as Capsule;
+use App\Exceptions\RouteNotFoundException;
 use App\Services\Emailable;
+use App\Services\AbstractApi;
+use Dotenv\Dotenv;
+use Illuminate\Container\Container;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Symfony\Component\Mailer\MailerInterface;
+use Twig\Environment;
+use Twig\Extra\Intl\IntlExtension;
+use Twig\Loader\FilesystemLoader;
 
 class App
 {
@@ -44,13 +47,27 @@ class App
 
         $this->initDb($this->config->db);
 
+        $twig = new Environment(
+            new FilesystemLoader(VIEW_PATH),
+            [
+                'cache' => STORAGE_PATH . '/cache',
+                'auto_reload' => true
+            ]
+        );
+
+        $twig->addExtension(new IntlExtension());
+
         $this->container->bind(MailerInterface::class, fn() => new CustomMailer($this->config->mailer['dsn']));
+        $this->container->singleton(Environment::class, fn() => $twig);
+
         $this->container->bind(
             EmailValidationInterface::class,
             fn() => new Emailable\EmailValidationService($this->config->apiKeys['emailable'])
         );
-
-        // $this->container->bind(EmailValidationInterface::class, fn() => new EmailValidationService($this->config->apiKeys['abstract_api_email_validation']));
+        //$this->container->bind(
+        //    EmailValidationInterface::class,
+        //    fn() => new AbstractApi\EmailValidationService($this->config->apiKeys['abstract_api_email_validation'])
+        //);
 
         return $this;
     }
--- app/Controllers/InvoiceController.php
+++ app/Controllers/InvoiceController.php
@@ -7,16 +7,31 @@ namespace App\Controllers;
 use App\Attributes\Get;
 use App\Enums\InvoiceStatus;
 use App\Models\Invoice;
-use App\View;
+use Twig\Environment AS Twig;
 
 class InvoiceController
 {
-    #[Get('/invoices')]
-    public function index(): View
+    public function __construct(private Twig $twig)
     {
-        $invoices =  Invoice::query()->where('status', InvoiceStatus::Paid)->get();
+    }
 
-        return View::make('invoices/index', ['invoices' => $invoices]);
+    #[Get('/invoices')]
+    public function index(): string
+    {
+        $invoices = Invoice::query()
+           ->where('status', InvoiceStatus::Paid)
+           ->get()
+           ->map(
+               fn(Invoice $invoice) => [
+                   'invoiceNumber' => $invoice->invoice_number,
+                   'amount'        => $invoice->amount,
+                   'status'        => $invoice->status->toString(),
+                   'dueDate'       => $invoice->due_date->toDateTimeString(),
+               ]
+           )
+           ->toArray();
+
+        return $this->twig->render('invoices/index.twig', ['invoices' => $invoices]);
     }
 
     #[Get('/invoices/new')]
