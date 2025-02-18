823318c0321371605f4f8275a4674235733eb627
Najibu <najibu.nsubuga@vice.com>
2025-02-18 11:14:20 +0000

    add emailable apis

--- app/App.php
+++ app/App.php
@@ -4,12 +4,14 @@ declare(strict_types = 1);
 
 namespace App;
 
-use App\Exceptions\RouteNotFoundException;
 use Dotenv\Dotenv;
-use Illuminate\Container\Container;
-use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+use App\Services\AbstractApi\EmailValidationService;
+use App\Exceptions\RouteNotFoundException;
+use App\Contracts\EmailValidationInterface;
 use Symfony\Component\Mailer\MailerInterface;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 class App
 {
@@ -42,6 +44,7 @@ class App
         $this->initDb($this->config->db);
 
         $this->container->bind(MailerInterface::class, fn() => new CustomMailer($this->config->mailer['dsn']));
+        $this->container->bind(EmailValidationInterface::class, fn() => new EmailValidationService($this->config->apiKeys['abstract_api_email_validation']));
 
         return $this;
     }
---
+++ app/Services/AbstractApi/EmailValidationService.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\AbstractApi;
+
+use RuntimeException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\HandlerStack;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\ConnectException;
+use App\Contracts\EmailValidationInterface;
+
+class EmailValidationService implements EmailValidationInterface
+{
+    private $baseUrl = "https://emailvalidation.abstractapi.com/v1/";
+    public function __construct(private string $apiKey)
+    {
+
+    }
+
+    public function verify(string $email): array
+    {
+        $stack = HandlerStack::create();
+
+        $maxRetries = 3;
+
+        $stack->push($this->getRetryMiddleware($maxRetries));
+
+        $client = new Client([
+            "base_uri"=> $this->baseUrl,
+            "timeout"=> 5,
+            "handler"=> $stack
+        ]);
+
+        $params = [
+            "api_key"=> $this->apiKey,
+            "email"=> $email
+        ];
+
+        $response = $client->get('', ['query', $params]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    private function getRetryMiddleware(int $maxRetries): callable
+    {
+        return Middleware::retry(function (int $retries, RequestInterface $request, ?ResponseInterface $response = null, ?RuntimeException $e = null) use ($maxRetries) {
+            if ($retries >= $maxRetries) {
+                return false;
+            }
+
+            if ($response && in_array($response->getStatusCode(), [249, 429, 503])) {
+                return true;
+            }
+
+            if ($e instanceof ConnectException) {
+                return true;
+            }
+
+            return false;
+        });
+    }
+}
