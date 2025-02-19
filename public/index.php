ff2ae72e2cb30a165d4c4192ee505ae8a08905de
Najibu <najibu.nsubuga@vice.com>
2025-02-19 12:43:45 +0000

    refactor: update HomeController to use Slim's Twig integration and clean up index.php

--- public/index.php
+++ public/index.php
@@ -2,13 +2,11 @@
 
 declare(strict_types = 1);
 
-use App\App;
-use App\Router;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Twig\Extra\Intl\IntlExtension;
 use App\Controllers\HomeController;
-use Illuminate\Container\Container;
 use App\Controllers\InvoiceController;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -16,27 +14,19 @@ require_once __DIR__ . '/../vendor/autoload.php';
 define('STORAGE_PATH', __DIR__ . '/../storage');
 define('VIEW_PATH', __DIR__ . '/../views');
 
-// $container = new Container();
-// $router    = new Router($container);
-
-// $router->registerRoutesFromControllerAttributes(
-//     [
-//         HomeController::class,
-//         InvoiceController::class,
-//     ]
-// );
-
-// (new App(
-//     $container,
-//     $router,
-//     ['uri' => $_SERVER['REQUEST_URI'], 'method' => $_SERVER['REQUEST_METHOD']]
-// ))->boot()->run();
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$app->get('/', [HomeController::class,'index']);
+$app->get('/invoice', [InvoiceController::class,'index']);
+
+$twig = Twig::create(VIEW_PATH, [
+    'cache'       => STORAGE_PATH . '/cache',
+    'auto_reload' => true,
+]);
+
+$twig->addExtension(new IntlExtension());
+
+$app->add(TwigMiddleware::create($app, $twig));
 
 $app->run();
